ed75d0ff51865319080876c593278c3e42994812
elquinator <63483720+elquinator@users.noreply.github.com>
2025-07-02 23:07:33 -0400

    error hanling an joining work, visual changes

--- index.js
+++ index.js
@@ -48,77 +48,93 @@ app.post("/games", (req, res) => {
 
 app.get("/game/:game_id", (req, res) => {
     fs.readFile(`state/games/${req.params.game_id}.json`, (err, data) => {
-        if (err) throw err;
-        const game = JSON.parse(data);
-        res.json(game);
+        if (err) {
+            res.json({'error':err})
+        }
+        else {
+            const game = JSON.parse(data);
+            res.json(game);
+        }
     });
 });
 
 app.put("/game/:game_id", (req, res) => {
     let playerIdentifier = '';
-    const game = JSON.parse(fs.readFileSync(`state/games/${req.params.game_id}.json`, (err, data) => {
-        if (err) throw err;
-    }));
-    const body = req.body;
-    const action = body.action;
-    switch (action) {
-        case "join":
-            const playerName = body.playerName;
-            if (game.playerX === '' && game.playerO === '') {
-                if (Math.random >= .5) {
+    let game;
+    try {
+        game = JSON.parse(fs.readFileSync(`state/games/${req.params.game_id}.json`, (err, data) => {
+            if (err) {
+                return {"error": "no such game"};
+            }
+        }));
+    } catch (e) {
+        game = {"error": e};
+    }
+    if (game.error) {
+        res.json(game);
+    }
+    else {
+        const body = req.body;
+        const action = body.action;
+        switch (action) {
+            case "join":
+                const playerName = body.playerName;
+                if (game.playerX === '' && game.playerO === '') {
+                    if (Math.random() >= .5) {
+                        game.playerX = playerName;
+                        playerIdentifier = 'X';
+                    }
+                    else {
+                        game.playerO = playerName;
+                        playerIdentifier = 'O';
+                    }
+                }
+                else if (game.playerX === '') {
                     game.playerX = playerName;
+                    game.gameStarted = true;
                     playerIdentifier = 'X';
                 }
-                else {
+                else if (game.playerO === '') {
                     game.playerO = playerName;
+                    game.gameStarted = true;
                     playerIdentifier = 'O';
                 }
-            }
-            else if (game.playerX === '') {
-                game.playerX = playerName;
-                game.gameStarted = true;
-                playerIdentifier = 'X';
-            }
-            else if (game.playerO === '') {
-                game.playerO = playerName;
-                game.gameStarted = true;
-                playerIdentifier = 'O';
+                else {
+                    res.json({
+                        "response":"game is full"
+                    });
+                    return;
+                }
+                res.json({
+                    "playerIdentifier": playerIdentifier
+                });
+                break;
+            case "move":
+                if (game.playerX === '' || game.playerO === '') {
+                    res.json({
+                        "response": "a player is missing"
+                    });
+                    return;
+                }
+                console.log(game.currentPlayer);
+                game.moves.push(body.move);
+                game.currentPlayer = body.newPlayer;
+                console.log(game.currentPlayer);
+                break;
+            default:
+                res.json({
+                    "response": `error action not recognized ${action}`
+                });
+                return;
+        }
+        fs.writeFile(`state/games/${req.params.game_id}.json`,JSON.stringify(game), (err) => {
+            if (err) {
+                console.error("move failed", err);
             }
             else {
-                res.json({
-                    "response":"game is full"
-                });
-                return;
+                console.log("move successful");
             }
-            res.json({
-                "playerIdentifier": playerIdentifier
-            });
-            break;
-        case "move":
-            if (game.playerX === '' || game.playerO === '') {
-                res.json({
-                    "response": "a player is missing"
-                });
-                return;
-            }
-            console.log(game.currentPlayer);
-            game.moves.push(body.move);
-            game.currentPlayer = body.newPlayer;
-            console.log(game.currentPlayer);
-            break;
-        default:
-            res.json({
-                "response": `error action not recognized ${action}`
-            });
-            return;
+        });
+        res.json(game);
     }
-    fs.writeFile(`state/games/${req.params.game_id}.json`,JSON.stringify(game), (err) => {
-        if (err) {
-            console.error("move failed", err);
-        }
-        else {
-            console.log("move successful");
-        }
-    });
-    res.json(game);
 });
